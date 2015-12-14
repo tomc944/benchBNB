e0aaaaa231018258cd5521f56e592b6ed69b4fcb
Thomas <thomas.claiborne3@gmail.com>
2015-12-14 10:06:07 -0800

    Added React Routes

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,13 +1,32 @@
 var React = require('react'),
     ReactDOM = require('react-dom'),
-    BenchStore = require('./stores/bench.js'),
-    apiUtil = require('./util/api_util.js'),
-    Search = require('./components/search');
+    Search = require('./components/search'),
+    Router = require('react-router').Router,
+    Route = require('react-router').Route,
+    IndexRoute = require('react-router').IndexRoute;
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <header><h1>Bench BnB</h1></header>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route path = "/" component={App}>
+    <IndexRoute component={Search} />
+  </Route>
+)
+
 
 // window.BenchStore = BenchStore;
 // window.apiUtil = apiUtil;
 
 window.addEventListener('DOMContentLoaded', function() {
   var root = document.getElementById('content');
-  ReactDOM.render(<Search />, root)
+  ReactDOM.render(<Router>{routes}</Router>, root)
 })
